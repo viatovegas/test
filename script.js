45b459a55c0231c90d3b50a80de94b1f4a1e0b8f
viatovegas <145833157+viatovegas@users.noreply.github.com>
2023-09-25 20:20:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // JavaScript
-console.log('Hello world!');
+
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+
+//背景色を変更する関数
+//function構文を使う書き方
+function changeColor(){
+  document.body.style.backgroundColor = color.value;
+  text.textContent = color.value;
+}
+
+//アロー関数を使う書き方
+// const changeColor = () => {
+//   document.body.style.backgroundColor = color.value;
+//   text.textContent = color.value;
+// }
+
+// カラーピッカーが変更されたときに、背景色を変える
+color.addEventListener('input', changeColor);
